959407126e99866eca0a7010377822a8d8811e80
faezehdev <faezeh0078@gmail.com>
2024-08-25 16:01:03 +0330

    complete section 5 of homepage

--- js/home.js
+++ js/home.js
@@ -40,3 +40,48 @@ let swiperBrand= new Swiper(".swiper-brands", {
     spaceBetween: 0,
       speed:1000
   });  
+
+// CUSOR FOLLOWER
+let cursor = $(".Cursor"),
+    follower = $(".Cursor-follower");
+let posX = 0,
+    posY = 0;
+
+    let mouseX = 0,
+    mouseY = 0;
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+       
+    TweenMax.set(follower, {
+        css: {    
+        left: posX - 12,
+        top: posY - 12
+        }
+    });
+    
+    TweenMax.set(cursor, {
+        css: {    
+        left: mouseX,
+        top: mouseY
+        }
+    });
+  }
+});
+
+document.querySelector('.ProductCats').addEventListener('mousemove',(e)=>{
+
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+})
+
+$(".ProductCats").on("mouseenter", function(e) {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$(".ProductCats").on("mouseleave", function(e) {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
