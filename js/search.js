e7cb0f8e44c3e6b47c902be4b140cac7a1bbcbdb
faezehdev <faezeh0078@gmail.com>
2024-08-26 19:30:27 +0330

    complete searchpage

---
+++ js/search.js
@@ -0,0 +1,26 @@
+// PRODUCT SLIDER
+let swiperProduct= new Swiper(".swiper-products", {
+    slidesPerView:4,
+    spaceBetween: 40,
+      speed:1000
+  });  
+  // PROJECT SLIDER
+let swiperProject= new Swiper(".swiper-projects", {
+    slidesPerView:2.4,
+    spaceBetween: 50,
+   speed:800
+  });  
+
+// news SLIDER
+let swiperNews= new Swiper(".swiper-news", {
+    slidesPerView:3.2,
+    spaceBetween: 30,
+   speed:800
+  });  
+
+// GALLERY SLIDER
+let swiperGallery= new Swiper(".swiper-gallery", {
+    slidesPerView:3.2,
+    spaceBetween: 30,
+   speed:800
+  });  
